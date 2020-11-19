6f022ad38339fa236e6b191cbe4e8da8a15bb32f
Alicia Shen <Alicia.Shen@c02y30gajg5j.myob.myobcorp.net>
2020-11-19 15:52:18 +1300

    [001][feat] Setting up project

---
+++ SumOddN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumOddN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a positive integer");
+        }
+    }
+}
