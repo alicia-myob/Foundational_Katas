517998a10bc30178e48344988c3c86f11fb7e150
Alicia Shen <Alicia.Shen@c02y30gajg5j.myob.myobcorp.net>
2020-11-19 14:31:37 +1300

    [002][feat] Detects Alice and Bob and greets

--- GreetAliceBob/Program.cs
+++ GreetAliceBob/Program.cs
@@ -6,7 +6,15 @@ namespace GreetAliceBob
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine();
+            if (name.Equals("Alice", StringComparison.OrdinalIgnoreCase)){
+                Console.WriteLine("Hello " + name);
+            } else if (name.Equals("Bob", StringComparison.OrdinalIgnoreCase)){
+                Console.WriteLine("Hello " + name);
+            } else {
+                Console.WriteLine("No greeting for you :(");
+            }
         }
     }
 }
