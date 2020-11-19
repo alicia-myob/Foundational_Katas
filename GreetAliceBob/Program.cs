aed57a62d2b043173b656ebadaf9861c26d40a6e
Alicia Shen <Alicia.Shen@c02y30gajg5j.myob.myobcorp.net>
2020-11-19 15:23:15 +1300

    [005][fix] Make input type string for exceptions

--- GreetAliceBob/Program.cs
+++ GreetAliceBob/Program.cs
@@ -4,24 +4,17 @@ namespace GreetAliceBob
 {
     class Program
     {
-        bool run = true;
+
         static void Main(string[] args)
         {
-            while (run){
-                Console.WriteLine("Enter your name: ");
-                var name = Console.ReadLine();
-                if (IsString(name)){
-                    if (name.Equals("Alice", StringComparison.OrdinalIgnoreCase)){
-                    Console.WriteLine("Hello " + name);
-                    } else if (name.Equals("Bob", StringComparison.OrdinalIgnoreCase)){
-                        Console.WriteLine("Hello " + name);
-                    } else {
-                        Console.WriteLine("No greeting for you :(");
-                    }
-                    run = false;
-                } else {
-                    Console.WriteLine("Not a valid string, please enter name again: ");
-                }
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            if (name.Equals("Alice", StringComparison.OrdinalIgnoreCase)){
+            Console.WriteLine("Hello " + name);
+            } else if (name.Equals("Bob", StringComparison.OrdinalIgnoreCase)){
+                Console.WriteLine("Hello " + name);
+            } else {
+                Console.WriteLine("No greeting for you :(");
             }
         }
     }
