c455436bc0a2df7e9a8bda0ac3aeb42b1f69e8fb
Alicia Shen <Alicia.Shen@c02y30gajg5j.myob.myobcorp.net>
2020-11-19 14:20:13 +1300

    [001][feat] Setting up project

---
+++ UserGreeting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserGreeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
