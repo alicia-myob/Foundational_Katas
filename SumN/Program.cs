072eb56a0acfa8244363bdb7c1b80bc02261cf6a
Alicia Shen <Alicia.Shen@c02y30gajg5j.myob.myobcorp.net>
2020-11-19 15:11:53 +1300

    [004][feat] Asking user to continue

--- SumN/Program.cs
+++ SumN/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace SumN
 {
@@ -6,20 +7,50 @@ namespace SumN
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a positive integer: ");
-            string n = Console.ReadLine();
-            int num = -1;
-            if (!int.TryParse(n, out num)){
-                Console.WriteLine("Not an integer");
-            } else {
-                num = Int32.Parse(n);
-                if (num == 0){
-                    Console.WriteLine("0");
+            bool run = true;
+            while (run){
+                bool ask = true;
+                Console.WriteLine("Enter a positive integer: ");
+                string n = Console.ReadLine();
+                int num = -1;
+                if (!int.TryParse(n, out num)){
+                    Console.WriteLine("Not an integer. Please enter a positive integer: ");
+                } else if (num < 0){
+                        Console.WriteLine("Negative number detected! Please enter a positive integer: ");
                 } else {
-
+                    num = Int32.Parse(n);
+                    
+                    if (num == 0){
+                        Console.WriteLine("0");
+                    } else {
+                        int[] array = new int[num];
+                        for (int i = 0; i < num; i++){
+                            array[i] = i+1;
+                        }
+                        int sumArray = array.Sum();
+                        Console.WriteLine("Sum: " + sumArray);
+                        while (ask){
+                            Console.WriteLine("Would you like to enter another number? (y/n): ");
+                            string cont = Console.ReadLine();
+                            if (cont.Equals("n", StringComparison.OrdinalIgnoreCase)){
+                                ask = false;
+                                run = false;
+                                Console.WriteLine("Thanks for using SumN!");
+                            } else if (cont.Equals("y", StringComparison.OrdinalIgnoreCase)){
+                                ask = false;
+                                run = true;
+                            } else {
+                                try {
+                                    throw new System.ArgumentException();
+                                } catch {
+                                    Console.WriteLine("Please type y for yes, n for no");
+                                    ask = true;
+                                }
+                            }
+                        }
+                    }
                 }
             }
-                
         }  
     }
 }
